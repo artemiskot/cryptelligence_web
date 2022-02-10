7ed265383c7118696bedce93e5321dd01e01dacb
artemiskot <61650907+artemiskot@users.noreply.github.com>
2022-02-10 17:25:58 +0300

    module changes?

--- next.config.js
+++ next.config.js
@@ -6,6 +6,17 @@
 // ]); // pass the modules you would like to see transpiled
 
 /** @type {import('next').NextConfig} */
-module.exports = {
+/* module.exports = {
   reactStrictMode: true,
-};
+}; */
+module.exports = {
+  webpack: config => {
+    const alias = { ...config.resolve.alias }
+    delete alias.url // alias to native-url
+    config.resolve = {
+      ...config.resolve,
+      alias
+    }
+    return config
+  }
+}
